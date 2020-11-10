57448e11c9afae16b4cd8b8c8ad4f25eb753af29
mcgillshakore <mcgills2093@gmail.com>
2020-11-09 22:48:00 -0600

    adding alternating character challenge

---
+++ Alternating Characters/altChar.js
@@ -0,0 +1,33 @@
+const s = "AAABBBAABB";
+        //012345678
+// "ABABABAB"
+// 6
+
+// Complete the alternatingCharacters function below.
+function alternatingCharacters(s) {
+    let totalDel = 0;
+    let a = s.split("")
+    // console.log(a)
+    for (let i = 0; i < a.length; i++){ // first index 0
+        // console.log(`This is the first loop ${a[i]}`)
+        // for(let j=0; j < a.length; j++){// second index 1
+        //     console.log(`This is the second loop ${a[j]}`)
+        // }
+        // console.log(`This is the first index of my array ${a[i]}${i} and this is the second index ${a[i + 1]}${i + 1}`)
+        if(a[i] === a[i +1]){
+            totalDel++;
+        }
+    }
+    return totalDel;
+}
+
+console.log(alternatingCharacters(s))
+// alternatingCharacters(s)
+
+arr = [ 10, 100, 60, 50]
+flightDur = 150
+
+boolean 
+function twoMovies( arr, flightDur){
+    
+}
