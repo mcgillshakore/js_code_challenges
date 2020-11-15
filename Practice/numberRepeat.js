1f77f8c52d3715ae2422e2361f732f465e1fef9d
mcgillshakore <mcgills2093@gmail.com>
2020-11-14 19:34:43 -0600

    adding practice folder

---
+++ Practice/numberRepeat.js
@@ -0,0 +1,47 @@
+// let n = [1,2,3,4,1,10,9,8,10,202,202,23,4,5,3]
+
+// function repeatingNumbers(arr){
+
+// }
+
+
+// function sumUp (n) { // Linear Time 
+//     let result = 0;
+//     for(let i=1; i<=n; i++){
+//         result = result + i;
+//     }
+//     return result;
+// }
+
+// let start = 0;
+// let end = 0;
+// console.log(sumUp(3));
+
+// function sumUp(n) { // Constant Time
+//     return (n/2) * (1 + n);
+// }
+
+// console.log(sumUp(2));
+
+
+// Practice Time!
+
+// Write an algorithm that takes an array of numbers as input and calculates the sum of those numbers.
+
+// Define the Time Complexity of that algorithm and determine what the lowest possible Time Complexity is for this problem.
+
+    let arr = [1,2,3,4]
+
+    // function sumNum(arr){ // Linear Time O(n)
+    //     let total = 0; // 1
+    //     for (const num of arr){ // 1
+    //        total += arr[i] // length of n or length of array
+    //     }
+    //     return total; // 1
+    // }
+
+    function sumNum(arr){
+
+    }
+
+    // console.log(sumArr(arr));
