56ffed349fb016e78a5917a9ce17c91e4b134820
mcgillshakore <mcgills2093@gmail.com>
2020-11-16 21:09:11 -0600

    adding overlapping_meetings

---
+++ Overlapping Meetings/overlapping_meetings.js
@@ -0,0 +1,21 @@
+const meetings = [[9,14], [17,20], [13, 15],]
+
+function mergeMeetings(meetings){
+    // let sorted = meetings.sort((a, b) => a[0] - b[0]);
+    // let mergedArray = []
+
+    // for(let i = 0; i < sorted.length; i++){
+    //     let j = 1
+    //     if(sorted[i][1] > sorted[j][0]){
+    //         mergedArray = [...mergedArray, [sorted[i][0], sorted[j][1]]]
+    //         i++;
+    //         j += 2
+    //     } else {
+    //         mergedArray = [...mergedArray, sorted[i]]
+    //     }
+    // }
+    // console.log(mergedArray)
+    // console.log(sorted)
+}
+
+mergeMeetings(meetings)
